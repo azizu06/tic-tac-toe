0d6807630b508e6bf89313f362c7839c601a54fc
azizu06 <ab725492@ucf.edu>
2025-11-22 14:48:36 -0500

    Create module for the game logic

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ const gameBoard = ( () => {
 
     const getMsg = () => message;
 
-    const restartGame = () => {
+    const restartGame = (name) => {
         for(let i = 0; i < board.length; i++){
             board[i] = '';
         }
-        message = 'Player X\'s turn';
+        message = `${name}'s turn`;
     };
 
     return {
@@ -55,15 +55,53 @@ const gameController = ( (
 
     const getActive = () => activePlayer;
 
+    const turnMessage = () => {
+        board.editMsg(`${getActive().name}'s turn`);
+    };
+
+    const checkWinner = () => {
+        const winCombos = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+        for(let i = 0; i < winCombos.length; i++){
+            let score = 0;
+            for(let j = 0; j < 3; j++){
+                if(board[winCombos[j]] === getActive().marker){
+                    score+=1
+                }
+                if(score === 3){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     const playRound = (index) => {
-        let round = 1;
+        if(board.getBoard(index) != ''){
+            return;
+        }
         board.editBoard(index, getActive().marker);
-        
+        if(checkWinner() === true){
+            board.editMsg(`${getActive().name} wins!`);
+            return;
+        }
+        if(!board.includes('')){
+            board.editMsg('Draw!');
+            return;
+        }
+        switchTurn();
+        turnMessage();
     };
+    turnMessage();
 
-
-    return {
-
-    };
+    return { playRound, getActive };
 
 })();
